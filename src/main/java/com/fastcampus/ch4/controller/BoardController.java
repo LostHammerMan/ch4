51740c2642547bea6644ba56ce142e0b67a133db
LostHammerMan <seong7577@naver.com>
2022-09-29 22:43:20 +0900

    Initial commit

---
+++ src/main/java/com/fastcampus/ch4/controller/BoardController.java
@@ -0,0 +1,137 @@
+package com.fastcampus.ch4.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import com.fastcampus.ch4.domain.BoardDto;
+import com.fastcampus.ch4.domain.PageHandler;
+import com.fastcampus.ch4.domain.SearchCondition;
+import com.fastcampus.ch4.service.BoardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+
+    @Autowired
+    BoardService boardService;
+
+    @GetMapping("/write")
+    public String write(Model m){
+        m.addAttribute("mode", "new");
+        return "board"; // 읽기와 쓰기에 사용, 쓰기에 사용할 떄는 mode = new
+    }
+
+    @PostMapping("/modify")
+    public String modify(BoardDto boardDto, Model m, HttpSession session, RedirectAttributes rttr){
+        String writer = (String) session.getAttribute("id");
+        boardDto.setWriter(writer);
+
+        try {
+            int rowCnt = boardService.modify(boardDto); // insert
+
+            if (rowCnt != 1) throw  new Exception("modify Failed");
+            rttr.addFlashAttribute("msg", "MOD_OK");
+            return "redirect:/board/list";
+        } catch (Exception e) {
+            e.printStackTrace();
+            m.addAttribute("boardDto", boardDto);
+            m.addAttribute("msg", "MOD_ERR");
+            return "board";
+        }
+
+    }
+
+    @PostMapping("/write")
+    public String write(BoardDto boardDto, Model m, HttpSession session, RedirectAttributes rttr){
+        String writer = (String) session.getAttribute("id");
+        boardDto.setWriter(writer);
+
+        try {
+            int rowCnt = boardService.write(boardDto); // insert
+
+            if (rowCnt != 1) throw  new Exception("Write Failed");
+            rttr.addFlashAttribute("msg", "WRT_OK");
+            return "redirect:/board/list";
+        } catch (Exception e) {
+            e.printStackTrace();
+            m.addAttribute("boardDto", boardDto);
+            m.addAttribute("msg", "WRT_ERR");
+            return "board";
+        }
+
+    }
+
+    @PostMapping("/remove")
+    public String remove(Integer bno, Integer page, Integer pageSize, Model m, HttpSession session, RedirectAttributes rttr){
+        String writer = (String)session.getAttribute("id");
+        try {
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+
+            int rowCnt = boardService.remove(bno, writer);
+
+            if (rowCnt != 1)
+                throw new Exception("board remove error");
+            rttr.addFlashAttribute("msg", "DEL_OK");
+        } catch (Exception e) {
+            e.printStackTrace();
+            rttr.addFlashAttribute("msg", "DEL_ERR"); // 한번 쓰고 없어짐
+        }
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/read")
+    public String read(Integer bno, Integer page, Integer pageSize, Model m){
+        try {
+            BoardDto boardDto = boardService.read(bno);
+//            m.addAttribute("boardDto", boardDto); // 아래 문장과 동일
+            m.addAttribute(boardDto);
+            m.addAttribute("page", page);
+            m.addAttribute("pageSize", pageSize);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "board";
+    }
+
+    @GetMapping("/list") // @ModelAttribute 자동으로 붙음
+    public String list(@ModelAttribute SearchCondition sc, Model m, HttpServletRequest request) {
+        if(!loginCheck(request))
+            return "redirect:/login/login?toURL="+request.getRequestURL();  // 로그인을 안했으면 로그인 화면으로 이동
+
+        try {
+
+            int totalCnt = boardService.getSearchResultCnt(sc);
+            PageHandler pageHandler = new PageHandler(totalCnt, sc);
+
+            List<BoardDto> list = boardService.getSearchResultPage(sc);
+            m.addAttribute("list", list);
+            m.addAttribute("ph", pageHandler);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "boardList"; // 로그인을 한 상태이면, 게시판 화면으로 이동
+    }
+
+    private boolean loginCheck(HttpServletRequest request) {
+        // 1. 세션을 얻어서
+        HttpSession session = request.getSession();
+        // 2. 세션에 id가 있는지 확인, 있으면 true를 반환
+        return session.getAttribute("id")!=null;
+    }
+}
---
+++ src/test/java/com/fastcampus/ch4/domain/PageHandlerTest.java
@@ -0,0 +1,22 @@
+//package com.fastcampus.ch4.domain;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.test.context.ContextConfiguration;
+//import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+//
+//import static org.junit.Assert.*;
+//
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/root-context.xml"})
+//public class PageHandlerTest {
+//
+//    @Test
+//    public void test(){
+//        PageHandler ph = new PageHandler(250,11);
+//        ph.print();
+//        System.out.println("ph= " + ph);
+//        assertTrue(ph.getBeginPage() ==11);
+//        assertTrue(ph.getEndPage() ==20);
+//    }
+//}
